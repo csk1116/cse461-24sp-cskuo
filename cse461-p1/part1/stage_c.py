b5a995f40cd316975a30ea526b98d9a7fec942b7
cskuo <cskuo@uw.edu>
2024-04-17 16:50:36 -0700

    modified bash for autograder

---
+++ cse461-p1/part1/stage_c.py
@@ -0,0 +1,13 @@
+def resolve_stage_c_packet(data):
+    pass
+
+def resolve_stage_d_packet(data):
+    pass
+
+def do_stage_c():
+    pass
+
+def do_stage_d():
+    pass
+
+# stage c and d virtual incoming packet
